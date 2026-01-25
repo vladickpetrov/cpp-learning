24199076032793f197b4025c912b44e76bd9825b
vladickpetrov <minnesota777@mail.ru>
2026-01-25 14:23:20 +0300

    added all essential functions: add, show and erase

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 void printMenu() {
     std::cout << "1. Add item \n";
@@ -8,30 +9,55 @@ void printMenu() {
     std::cout << "4. Exit \n";
 }
 
-void handleInput(int& user_input) {
-    std::cout << "Choose a number: ";
+void handleInput(int &user_input) {
+    std::cout << "Enter a number: \n";
     std::cin >> user_input;
 }
 
-void addItem() {
+void addItem(std::vector<int> &items, int &item) {
+    handleInput(item);
+    items.push_back(item);
 }
 
-void showItems() {
+void showItems(const std::vector<int> &items) {
+    for (auto item: items) {
+        std::cout << item << " ";
+    }
+    std::cout << '\n';
 }
 
-void removeItem() {
+void removeItem(std::vector<int> &items, int &item) {
+    handleInput(item);
+    if (std::find(items.begin(), items.end(), item) == items.end()) {
+        std::cout << "There is no " << item << " in the list \n";
+        return;
+    }
+    auto new_start = std::remove(items.begin(), items.end(), item);
+    items.erase(new_start, items.end());
+    std::cout << "New list is:\n";
+
+    showItems(items);
 }
 
 int main() {
+    std::vector<int> items;
     int choise;
+    int item;
 
     while (true) {
         printMenu();
         handleInput(choise);
 
-        if (choise == 1) continue;
-        if (choise == 2) continue;
-        if (choise == 3) continue;
-        if (choise == 4) break;
+        if (choise == 1) {
+            addItem(items, item);
+        } else if (choise == 2) {
+            showItems(items);
+        } else if (choise == 3) {
+            removeItem(items, item);
+        } else if (choise == 4) {
+            break;
+        } else {
+            std::cout << "It's not an option \n";
+        }
     }
 }
