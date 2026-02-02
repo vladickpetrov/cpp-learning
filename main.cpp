31646f6db6b865c7c162446f10fcf468dcdf0254
vladickpetrov <minnesota777@mail.ru>
2026-02-02 12:48:11 +0300

    added test for essential functions

--- main.cpp
+++ main.cpp
@@ -37,14 +37,30 @@ void printMenu() {
 }
 
 void run_test(const std::string& test_name, bool (*test_func)()) {
-    if (test()) {
+    if (test_func()) {
         std::cout << "[OK]   " << test_name << '\n';
     } else {
         std::cout << "[FAIL]   " << test_name << '\n';
     }
 }
 
-bool test_empty_on_strat(){}
+bool test_empty_on_start() {
+    ItemList list("./tests/test_empty.txt");
+    return list.empty();
+}
+
+bool test_add() {
+    ItemList list("./tests/test_add");
+    list.add(1);
+    return !list.empty() && list.items().size() == 1;
+}
+
+bool test_remove_existing() {
+    ItemList list("./tests/test_remove_existing");
+    list.add(3);
+    list.remove_value(3);
+    return list.empty();
+}
 
 int readInput(const std::string &prompt) {
     int input;
@@ -111,31 +127,36 @@ bool ItemList::remove_value(int value) {
 }
 
 int main() {
-    ItemList list("items.txt");
 
-    while (true) {
-        printMenu();
-        int choice = readInput("Enter a menu item: ");
+    run_test("add one element", test_add);
+    run_test("empty on start", test_empty_on_start);
+    run_test("remove existing", test_remove_existing);
 
-        if (choice == 1) {
-            int add_value = readInput("Enter an element you want to add: ");
-            list.add(add_value);
-        } else if (choice == 2) {
-            if (list.empty()) {
-                std::cout << "List is empty\n";
-            } else {
-                for (auto item: list.items()) {
-                    std::cout << item << " ";
-                }
-                std::cout << '\n';
-            }
-        } else if (choice == 3) {
-            int rem_value = readInput("Enter an element which all instances you want to delete: ");
-            list.remove_value(rem_value);
-        } else if (choice == 4) {
-            break;
-        } else {
-            std::cout << "It's not an option \n";
-        }
-    }
+    // ItemList list("items.txt");
+    //
+    // while (true) {
+    //     printMenu();
+    //     int choice = readInput("Enter a menu item: ");
+    //
+    //     if (choice == 1) {
+    //         int add_value = readInput("Enter an element you want to add: ");
+    //         list.add(add_value);
+    //     } else if (choice == 2) {
+    //         if (list.empty()) {
+    //             std::cout << "List is empty\n";
+    //         } else {
+    //             for (auto item: list.items()) {
+    //                 std::cout << item << " ";
+    //             }
+    //             std::cout << '\n';
+    //         }
+    //     } else if (choice == 3) {
+    //         int rem_value = readInput("Enter an element which all instances you want to delete: ");
+    //         list.remove_value(rem_value);
+    //     } else if (choice == 4) {
+    //         break;
+    //     } else {
+    //         std::cout << "It's not an option \n";
+    //     }
+    // }
 }
